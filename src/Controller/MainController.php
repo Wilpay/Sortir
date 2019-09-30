6262c3c58ddffb1d1e91c8155247adb7538e3bdf
Wilpay <willytenaud gmail.com>
2019-09-30 13:13:11 +0200

    initialisation

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/", name="main_home")
+     */
+    public function home()
+    {
+        return $this->render("main/home.html.twig");
+    }
+}
