e1b5cb5c9464aad5789e9ab17dc9aeca44e11ca5
Wilpay <willytenaud gmail.com>
2019-10-01 09:46:37 +0200

    Refonte des classes Entity 'Sortie'

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -81,6 +81,224 @@ class Sortie
      */
     private $inscrit;
 
+    /**
+     * @var \App\Entity\Site
+     *
+     * @ORM\ManyToMany(targetEntity="App\Entity\Site", inversedBy="siteOrganisateur")
+     * @ORM\JoinColumn(name="idSite", referencedColumnName="idSortie")
+     */
+    private $siteOrganisateur;
+
+    /**
+     * @return mixed
+     */
+    public function getIdSortie()
+    {
+        return $this->idSortie;
+    }
+
+    /**
+     * @param mixed $idSortie
+     */
+    public function setIdSortie($idSortie)
+    {
+        $this->idSortie = $idSortie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateHeureDebut()
+    {
+        return $this->dateHeureDebut;
+    }
+
+    /**
+     * @param mixed $dateHeureDebut
+     */
+    public function setDateHeureDebut($dateHeureDebut)
+    {
+        $this->dateHeureDebut = $dateHeureDebut;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDuree()
+    {
+        return $this->duree;
+    }
+
+    /**
+     * @param mixed $duree
+     */
+    public function setDuree($duree)
+    {
+        $this->duree = $duree;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateLimiteInscription()
+    {
+        return $this->dateLimiteInscription;
+    }
+
+    /**
+     * @param mixed $dateLimiteInscription
+     */
+    public function setDateLimiteInscription($dateLimiteInscription)
+    {
+        $this->dateLimiteInscription = $dateLimiteInscription;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNbInscriptionsMax()
+    {
+        return $this->nbInscriptionsMax;
+    }
+
+    /**
+     * @param mixed $nbInscriptionsMax
+     */
+    public function setNbInscriptionsMax($nbInscriptionsMax)
+    {
+        $this->nbInscriptionsMax = $nbInscriptionsMax;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInfosSortie()
+    {
+        return $this->infosSortie;
+    }
+
+    /**
+     * @param mixed $infosSortie
+     */
+    public function setInfosSortie($infosSortie)
+    {
+        $this->infosSortie = $infosSortie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEtat()
+    {
+        return $this->etat;
+    }
+
+    /**
+     * @param mixed $etat
+     */
+    public function setEtat($etat)
+    {
+        $this->etat = $etat;
+    }
+
+    /**
+     * @return Lieu
+     */
+    public function getLieu(): Lieu
+    {
+        return $this->Lieu;
+    }
+
+    /**
+     * @param Lieu $Lieu
+     */
+    public function setLieu(Lieu $Lieu)
+    {
+        $this->Lieu = $Lieu;
+    }
+
+    /**
+     * @return Etat
+     */
+    public function getEtats(): Etat
+    {
+        return $this->Etats;
+    }
+
+    /**
+     * @param Etat $Etats
+     */
+    public function setEtats(Etat $Etats)
+    {
+        $this->Etats = $Etats;
+    }
+
+    /**
+     * @return Participant
+     */
+    public function getOrganise(): Participant
+    {
+        return $this->Organise;
+    }
+
+    /**
+     * @param Participant $Organise
+     */
+    public function setOrganise(Participant $Organise)
+    {
+        $this->Organise = $Organise;
+    }
+
+    /**
+     * @return Participant
+     */
+    public function getInscrit(): Participant
+    {
+        return $this->inscrit;
+    }
+
+    /**
+     * @param Participant $inscrit
+     */
+    public function setInscrit(Participant $inscrit)
+    {
+        $this->inscrit = $inscrit;
+    }
+
+    /**
+     * @return Site
+     */
+    public function getSiteOrganisateur(): Site
+    {
+        return $this->siteOrganisateur;
+    }
+
+    /**
+     * @param Site $siteOrganisateur
+     */
+    public function setSiteOrganisateur(Site $siteOrganisateur)
+    {
+        $this->siteOrganisateur = $siteOrganisateur;
+    }
+
+
+
 
 
 
