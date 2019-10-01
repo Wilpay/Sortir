19d69d695da787509593cf7f8cd91cb13ffa1720
qbaudry <quentin.baudry2018@gmail.com>
2019-10-01 11:20:04 +0200

    Connexion

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,17 +4,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends Controller
 {
     /**
      * @Route("/connexion", name="connexion")
      */
-    public function connexion()
+    public function connexion(AuthenticationUtils $authenticationUtils, Request $request)
     {
-        return $this->render("user/connexion.html.twig");
+        $errors = $authenticationUtils->getLastAuthenticationError();
+        $lastname = $authenticationUtils->getLastUsername();
+
+        dump($this->getUser());
+
+        return $this->render("user/connexion.html.twig", [
+            'lastusername' => $lastname,
+            'error' => $errors
+        ]);
     }
+
+    /**
+     * @Route("/deconnexion", name="deconnexion")
+     */
+    public function deconnexion(){}
 
     /**
      * @Route("/monProfil", name="monProfil")
@@ -22,5 +37,13 @@ class UserController extends Controller
     public function monProfil()
     {
         return $this->render("user/mon-profil.html.twig");
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function profil()
+    {
+        return $this->render("user/profil.html.twig");
     }
 }
