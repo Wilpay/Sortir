8aac51e578a2c2d329dcb10ed5dd4f71cb5786d4
Wilpay <willytenaud gmail.com>
2019-10-02 09:47:29 +0200

    Tableau Sortie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +13,14 @@ class HomeController extends Controller
     /**
      * @Route("/", name="home")
      */
-    public function home()
+    public function home(EntityManagerInterface $em)
     {
-        return $this->render("main/home.html.twig");
+        $sortie = $em->getRepository(Sortie::class)->findAll();
+
+        return $this->render("main/home.html.twig", [
+            'sorties' => $sortie,
+        ]);
+
     }
 
 }
