7614a59b1e92812666d28cbe79d659a3f11cdfc7
qbaudry <quentin.baudry2018@gmail.com>
2019-10-02 11:04:49 +0200

    Modification template Form

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -33,6 +34,7 @@ class ParticipantType extends AbstractType
                 'choice_label' => 'nom'
             ))
             ->add('passwordPlain', PasswordType::class)
+            ->add('submit', SubmitType::class)
         ;
     }
 
