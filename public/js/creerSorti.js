7e91154c1d78f4eb674c215f6560eab566a7efc1
Blelodet <lelodet.b@gmail.com>
2019-10-09 12:02:36 +0200

    construction requete tri des sorties

--- public/js/creerSorti.js
+++ public/js/creerSorti.js
@@ -23,9 +23,9 @@ function getLieux($idVille){
         type: 'POST',
         url: $("#urlLieu").val(),
         data: "&idVille="+$idVille,
-        complete: function(data) {
+        success: function(data) {
             //console.log(data.responseText);
-            $("#lieu").html(data.responseText);
+            $("#lieu").html(data);
             getInfosLieux($("#sortie_lieu").val());
         }
     });
@@ -36,9 +36,27 @@ function getInfosLieux($idLieu){
         type: 'POST',
         url: $("#urlInfoLieu").val(),
         data: "&idLieu="+$idLieu,
-        complete: function(data) {
+        success: function(data) {
             //console.log(data.responseText);
-            $("#infoLieu").html(data.responseText);
+            $("#infoLieu").html(data);
+        },
+        error:function(data) {
+            //console.log(data.responseText);
+            $("#infoLieu").html('<div class="row mb-2">\n' +
+                '    <div class="font-weight-bold col-4">Rue:</div>\n' +
+                '    <div class="col-8">\n' +
+                '    </div>\n' +
+                '</div>\n' +
+                '<div class="row mb-2">\n' +
+                '    <div class="font-weight-bold col-4">Latitude:</div>\n' +
+                '    <div class="col-8">\n' +
+                '    </div>\n' +
+                '</div>\n' +
+                '<div class="row mb-2">\n' +
+                '    <div class="font-weight-bold col-4">Longitude:</div>\n' +
+                '    <div class="col-8">\n' +
+                '    </div>\n' +
+                '</div>' );
         }
     });
 }
